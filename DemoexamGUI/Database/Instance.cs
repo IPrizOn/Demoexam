b8f7e8b67f5d34d6d8e904fad47b631a8af3e320
IPrizOn <ipygachev333@gmail.com>
2025-02-20 23:47:45 +0200

    Add: base structure

---
+++ DemoexamGUI/Database/Instance.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoexamGUI.Database
+{
+    internal class Instance
+    {
+        private string connection;
+
+        public string GetConnection()
+        {
+            return connection;
+        }
+    }
+}
---
+++ DemoexamGUI/Domain/Partner.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoexamGUI.Domain
+{
+    internal class Partner
+    {
+        int ID { get; set; }
+        string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
